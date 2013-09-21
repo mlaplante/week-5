51c4a0730c1533925b12751b27fa411653453a5d
Michael La Plante <michael@michaellaplante.com>
2013-09-21 11:43:46 -0400

    ading modal view controller

---
+++ Storyboard/LPModalViewController.h
@@ -0,0 +1,13 @@
+//
+//  LPModalViewController.h
+//  Storyboard
+//
+//  Created by Michael La Plante on 9/21/13.
+//  Copyright (c) 2013 Michael La Plante. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LPModalViewController : UIViewController
+
+@end
---
+++ Storyboard/LPModalViewController.m
@@ -0,0 +1,38 @@
+//
+//  LPModalViewController.m
+//  Storyboard
+//
+//  Created by Michael La Plante on 9/21/13.
+//  Copyright (c) 2013 Michael La Plante. All rights reserved.
+//
+
+#import "LPModalViewController.h"
+
+@interface LPModalViewController ()
+
+@end
+
+@implementation LPModalViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
